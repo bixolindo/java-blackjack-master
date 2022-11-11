76b814430080821079b9e50ebf539b8310858dd5
Ian Carlos <42481994+BahiaIan@users.noreply.github.com>
2022-11-10 22:00:55 -0300

    removendo condicionais desnecessárias, otimizando vizualização

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -3,32 +3,25 @@
 
 import javax.swing.UIManager;
 
-public class Blackjack
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
-        
-        /**
-         * I think that an application should adopt the same look and feel as 
-         * the system it's running on. This is what a user expects from
-         * an application. It's great that Java is cross platform and all,
-         * but the end-user doesn't care if a program is written in Java
-         * or not.
-         * So, I force the application to use the system look and feel. If
-         * the look and feel can't be found, it'll use metal anyway as a last
-         * resort.
-         */
-        
-        System.setProperty("apple.laf.useScreenMenuBar", "true");
-        
-        AppWindow window = new AppWindow();      
-    }
+public class Blackjack {
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		/**
+		 * I think that an application should adopt the same look and feel as the system
+		 * it's running on. This is what a user expects from an application. It's great
+		 * that Java is cross platform and all, but the end-user doesn't care if a
+		 * program is written in Java or not. So, I force the application to use the
+		 * system look and feel. If the look and feel can't be found, it'll use metal
+		 * anyway as a last resort.
+		 */
+
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+
+		AppWindow window = new AppWindow();
+	}
 }
